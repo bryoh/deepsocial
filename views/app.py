aadcb515d75be972e365e6a4a7313891909adefa
bryoh <brianbryo@gmail.com>
2017-04-09 14:33:17 +0100

    Pip issues fix

--- views/app.py
+++ views/app.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template
-from chart_examples import chart_example 
+from chart_examples import create_chart_elements
+from pprint import pprint
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    div, script = chart_example()
+    ret = create_chart_elements('test', ["#E24736", "#FF9438", "#FFF249"], [5, 5.5, 10], "PolarArea")
+
+    div = ret['canvas'] 
+    script= ret['body_script']
+    dom_manipulation_script=ret['dom_script']
+    pprint(div)
+    pprint(script)
+    pprint(dom_manipulation_script)
     return render_template('index_.html', 
                            div=div, 
-                           script=script)
+                           script=script,
+                          dom= dom_manipulation_script)
 
 
 if __name__ == '__main__':

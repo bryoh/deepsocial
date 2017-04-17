9d10cde7f7313295a0759149885fca7b9cea2fd8
bryoh <brianbryo@gmail.com>
2017-04-17 14:52:53 +0100

    [CHART] Finished the chart constructor module
    Now we should be able to create a chart using one line and get three different html emelents i.e. canvas, script, and dom-manipulation scipt
    which can be parsed/rendered"

--- views/chart_examples.py
+++ views/chart_examples.py
@@ -1,8 +1,14 @@
+'''
+chart arguments dictionary
+
+ret = {'name': None, 'chart_type': None, 'labels': None}
+
+'''
 import chartjs
 from pprint import pprint as pp
 from bs4 import BeautifulSoup
 
-def chart_constructor(args_dict=None):
+def example(args_dict=None):
     ''' TODO: Modify this so that is accepts a dictionary with 4 keys '''
     # Make a pie chart
     mychart = chartjs.chart("Sample pie chart", "PolarArea")  # args_dict['name'] args_dict['chart_type']
@@ -12,6 +18,19 @@ def chart_constructor(args_dict=None):
     mychart.set_highlights(["#E07369", "#FFAC68", "#FFF293"])
     mychart.add_dataset([5,2.3,10])  # args_dict['dataset']
     return mychart.make_chart()
+
+
+def chart_constructor(args_dict):
+    name= args_dict['name']
+    labels= args_dict['labels']
+    dataset = args_dict['dataset']
+    chart_type = args_dict['chart_type']
+    chart_obj = chartjs.chart(name, chart_type)
+    chart_obj.set_labels(labels)
+    chart_obj.set_colors(["#E24736", "#FF9438", "#FFF249"])  
+    chart_obj.set_highlights(["#E07369", "#FFAC68", "#FFF293"])
+    chart_obj.add_dataset(dataset)
+    return chart_obj.make_chart()
 
 
 def create_soup(html_str):
@@ -28,7 +47,9 @@ def get_DOM_tags(soup, tags):
 
 def return_chartjs_dic(list_of_html_tags):
     ''' returns a dictionary of 3 keys which are canvas, fuction script(with data set) and function script for dom Manipulation '''
-    pass
+    ret = {'canvas': list_of_html_tags[0], 'body_script': list_of_html_tags[1], 'dom_script': list_of_html_tags[2]}
+    return ret
+
 
 
 def random_colour():
@@ -36,21 +57,26 @@ def random_colour():
     # TODO: Expand this function so that it generates a random colour from a range
     pass
 
+
 def create_arg_dic(chart_name, chart_labels, chart_data_set, chart_type):
     ''' create a dictionary to use as the argument for the chart constructor '''
-    pass # returns a dict
+    return {'name': chart_name, 'labels': chart_labels, 'dataset':
+           chart_data_set, 'chart_type': chart_type}
 
 
 def create_chart_elements(chart_name, chart_labels, chart_data_set, chart_type):
     ''' main function that combines all the tools into one '''
-    pass
+    args_dict = create_arg_dic(chart_name, chart_labels, chart_data_set, chart_type)
+    html_str = chart_constructor(args_dict)
+    soup = create_soup(html_str)
+    list_of_html_tags = get_DOM_tags(soup, ['canvas', 'script'])
+    return return_chartjs_dic(list_of_html_tags)
+    
+    
 
 
 if __name__ == '__main__':
-    ret= chart_constructor()
+    ret= example()
     import pdb; pdb.set_trace()
-    1+1
-    soup = create_soup(ret)
-    tags_list = get_DOM_tags(soup, ['canvas', 'script', 'script'])
-    print(tags_list)
-
+    elements = create_chart_elements('test', ["#E24736", "#FF9438", "#FFF249"], [5, 5.5, 10], "PolarArea")
+    pp(elements)

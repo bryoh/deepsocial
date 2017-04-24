67a81f5bc968592c7d02372fc113c7d5b5fedd97
bryoh <brianbryo@gmail.com>
2017-04-24 20:03:07 +0100

    Added tests, html headers for bootstrap, chartjs and jcloud

--- views/chart_examples_test.py
+++ views/chart_examples_test.py
@@ -1,7 +1,31 @@
 import deepsocial.views.chart_examples as fut
 
+chart_name = 'test'
+chart_labels = 'blue', 'red', 'white'
+chart_data_set = 12, 15, 200
+chart_type = 'PolarArea'
+
 
 def test_random_colour():
     given = 'blah'
     expected = 'blah'
     assert fut.random_colour() == expected 
+
+
+def test_create_arg_dic():
+    ret = fut.create_arg_dic(chart_name, chart_labels, chart_data_set, chart_type)
+    expected = 'name', 'labels', 'dataset', 'chart_type'
+    ret_keys = ret.keys()
+    print(list(ret.keys()))
+    assert set(ret_keys) == set(expected), 'Expected list of keys is correct'
+    assert len(ret_keys) == len(expected), 'Number of keys'
+
+
+def test_create_chart_elements():
+    ret = fut.create_chart_elements(chart_name, chart_labels, chart_data_set, chart_type)
+    expected = 'canvas', 'body_script', 'dom_script'
+    ret_keys = ret.keys()
+    assert set(ret_keys) == set(expected), 'Keys'
+    assert len(ret_keys) == len(expected), 'Number of keys'
+
+

8223f37dce2ab66b3519b6858cb5c3f77b50a070
bnyamu <brianbryo@gmail.com>
2017-07-18 23:55:02 +0100

    Front End: Debug off

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from views import app
 app = app.app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- views/app.py
+++ views/app.py
@@ -106,5 +106,3 @@ def index():
                                )
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
